b362f36aef2f392ddcd186249c6088f15a71adfd
031la22-tech <031la22@upch.mx>
2026-05-14 14:23:25 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = "sql5.freesqldatabase.com";
-
 $usuario = "sql5826887";
-
 $password = "sdAucDGhT3";
-
 $bd = "sql5826887";
 
 $conn = mysqli_connect(
@@ -20,5 +20,7 @@ if(!$conn){
     die("Error de conexión: " . mysqli_connect_error());
 
 }
+
+echo "Conexión exitosa";
 
 ?>
